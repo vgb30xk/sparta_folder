3408d3a70ee127f47ceebde22ce66ea0f02c8717
Jaehyun_Park <115146172+vgb30xk@users.noreply.github.com>
2022-11-26 14:04:29 +0900

    Delete Aclass_7group_project directory

--- Aclass_7group_project/js/util.js
+++
@@ -1,5 +0,0 @@
-export const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-export const pwRegex =
-  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-
-export const getYYYYMMDD = (now) => new Date(now).toISOString().split("T")[0];
